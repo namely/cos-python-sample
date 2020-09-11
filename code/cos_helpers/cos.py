66e9fca2e4080e8eb4bcb443e57f660c779d7176
zen <zen.yui@namely.com>
2020-09-11 01:08:33 -0400

    works

--- code/cos_helpers/cos.py
+++ code/cos_helpers/cos.py
@@ -1,23 +1,28 @@
-from chief_of_state.v1.writeside_pb2 import PersistAndReply, Reply, HandleEventResponse
-from chief_of_state.v1.writeside_pb2 import HandleCommandResponse, HandleEventResponse
+from chief_of_state.v1.writeside_pb2 import HandleCommandResponse
 from google.protobuf.any_pb2 import Any
+from google.protobuf.empty_pb2 import Empty
 
 
-class CosEventReplyTypes():
+class CosCommandResponses():
     @staticmethod
     def persist_and_reply(event):
-        '''wrap event in command response with COS instruction PersistAndReply'''
-        any_event = Any()
-        any_event.Pack(event)
-
-        persist_and_reply = PersistAndReply()
-        persist_and_reply.event.CopyFrom(any_event)
-
-        response = HandleCommandResponse(persist_and_reply=persist_and_reply)
-
-        return response
+        return CosCommandResponses.event(event)
 
     @staticmethod
     def reply():
         '''helper method for generating a reply message'''
-        return HandleCommandResponse(reply=Reply())
+        return CosCommandResponses.no_event()
+
+
+    @staticmethod
+    def event(event):
+        '''helper method for generating an event message'''
+        any_event = Any()
+        any_event.Pack(event)
+        response = HandleCommandResponse(event=any_event)
+        return response
+
+    @staticmethod
+    def no_event():
+        '''helper method for generating a no event message'''
+        return HandleCommandResponse(no_event=Empty())
